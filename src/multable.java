c568bb9fe3e5276708a5f33a49e49fd3feda86b0
7odafarghal <7odafarghal@gmail.com>
2017-08-29 04:03:44 +0200

    finished challenge muliplication table at stepik

--- src/multable.java
+++ src/multable.java
@@ -2,12 +2,34 @@ import java.util.*;
 public class multable {
     public static void main(String[] args){
         Scanner masih = new Scanner(System.in);
-        int a = 1;
-        int b = 3;
-        int c = 2;
-        int d = 4;
-        int[] col = new int[(b+1)-a];
-        int[] row = new int[(d+1)-c];
+        int a = masih.nextInt();
+        int b = masih.nextInt();
+        int collngth = b-a+1;
+        int c = masih.nextInt();
+        int d = masih.nextInt();
+        int rowlngth = d-c+1;
+
+        int[] col = new int[collngth];
+        for(int i=0;i<col.length;i++){
+            col[i] =a+i;
+        }
+
+        int[] row = new int[rowlngth];
+        for(int i=0;i<row.length;i++){
+            row[i] =c+i;
+        }
+
+        System.out.print(" \t");
+        for(int i=0;i<row.length;i++)System.out.print(row[i]+"\t");
+        System.out.println();
+
+        for(int i=0;i<col.length;i++){
+            System.out.print(col[i]+"\t");
+            for(int j=0;j<row.length;j++){
+                System.out.print(col[i]*row[j] +"\t");
+            }
+            System.out.println();
+        }
 
     }
 }
