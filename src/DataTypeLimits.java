8fcc188a70fc136635a102f410edec58f61f91e9
7odafarghal <7odafarghal@gmail.com>
2017-09-06 20:21:26 +0200

    hackerRant version

--- src/DataTypeLimits.java
+++ src/DataTypeLimits.java
@@ -5,25 +5,26 @@ public class DataTypeLimits {
         int count=masih.nextInt();
         for (int i = 0; i < count; i++) {
             try {
-
                     long num = masih.nextLong();
-                    System.out.println(num + " can be fitted in:");
-                    if (num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE) {
+                    System.out.println(num +" can be fitted in:");
+                    if(num >= Byte.MIN_VALUE&&num <= Byte.MAX_VALUE) {
                         System.out.println("* byte");
                         System.out.println("* short");
                         System.out.println("* int");
                         System.out.println("* long");
-                    } else if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE) {
+                    }
+                    else if(num >= Short.MIN_VALUE&&num <= Short.MAX_VALUE) {
                         System.out.println("* short");
                         System.out.println("* int");
                         System.out.println("* long");
-                    } else if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE) {
+                    }
+                    else if(num >= Integer.MIN_VALUE&&num <= Integer.MAX_VALUE) {
                         System.out.println("* int");
                         System.out.println("* long");
-                    } else System.out.println("* long");
+                    }
+                    else System.out.println("* long");
 
-            } catch(Exception e)
-            {
+            } catch(Exception e) {
                 System.out.println(masih.next()+" can't be fitted anywhere.");
             }
         }
