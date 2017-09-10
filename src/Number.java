f8668fd848ff8dba8ccf95cfcf25b32977097867
7odafarghal <7odafarghal@gmail.com>
2017-09-10 20:10:25 +0200

    fixed the negative cases in Number

--- src/Number.java
+++ src/Number.java
@@ -1,20 +1,37 @@
-import java.util.*;
 public class Number {
-
-    public static int power(int num,int pow){
-        int numb =num;
-        for (int i = 0; i <= pow; i++) {
-            if(i==0){numb=1;continue;}
-            numb = numb * num;
+    //positive powers
+    public static int Power(int num,int pow){
+        int numb = num;
+        if(pow==0)numb = 1;
+        else {
+            for (int i = 0; i <= Math.abs(pow); i++) {
+                if (i == 0) {
+                    numb = 1;
+                    continue;
+                }
+                numb = numb * num;
+            }
         }
         return numb;
     }
+    //negative powers
+    public static double Power(int num,double pow){
+        int numb =num;
+        double numbd;
+        for (int i = 0; i <= Math.abs(pow); i++) {
+            if(i==0){numb=1;continue;}
+            numb = numb * num;
+        }
+        if(pow<0){numbd=(double) 1/numb; return numbd;}
+        else if(pow>0)return numb;
+        else return 1;
+    }
 
-    //this series is(num^0+num^1..........+num^pow)
-    public static int seriesOfPowers(int num ,int maxPow){
+    //this series is(num^0+num^1..........+num^pow) takes only positive powers
+    public static int seriesOfpPowers(int num ,int maxpPow){
         int numb=num;
         int zwei=0;
-        for (int i = 0; i < maxPow; i++) {
+        for (int i = 0; i < maxpPow; i++) {
             for (int j = 0; j <= i; j++) {
                 if(j==0){numb=1;continue;}
                 numb = numb * num;
@@ -23,8 +40,7 @@ public class Number {
             zwei+=numb;
         }
         System.out.print("= ");
-        if(maxPow==0) return 1;
-        else if(maxPow<0)return 1/zwei;
+        if(maxpPow==0) return 1;
         else return zwei;
     }
 }
--- src/SomeFcknSeries.java
+++ src/SomeFcknSeries.java
@@ -4,7 +4,7 @@ public class SomeFcknSeries {
         Scanner masih=new Scanner(System.in);
             int n=masih.nextInt();
             for (int k = 0; k < n; k++) {
-                System.out.print(Number.seriesOfPowers(2,k + 1));//adding one here because in that method there the num is already less than n by 1 so if we give it k which is less than n by 1 num would be less than n by 2
+                System.out.print(Number.seriesOfpPowers(2,k + 1));//adding one here because in that method there the num is already less than n by 1 so if we give it k which is less than n by 1 num would be less than n by 2
                 System.out.print(" ");
             }
         System.out.println();
