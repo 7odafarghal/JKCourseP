21964c96e5132fe257f94e44292841bf4d491f96
Mahmoud Farghal <7odafarghal@gmail.com>
2018-03-18 21:47:02 +0200

    no turning back baby

--- gboard.java
+++ gboard.java
@@ -1,5 +1,9 @@
 public class gboard{
     public static void main(String[] args) {
         System.out.print("best keyboard");
+        if(4<3){ 
+            return;
+            System.out.print("no turning back baby");
+        }
     }
 }
