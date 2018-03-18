cac5629f421842b397150082b525e1ff3e31d674
Mahmoud Farghal <7odafarghal@gmail.com>
2018-03-18 21:05:38 +0200

    added v1 message

--- src/kika.java
+++ src/kika.java
@@ -1,5 +1,8 @@
 public class kika{
     public static void main(String[] args){
         System.out.print("what is that keyboard");
+        if (keyboard.version = 1){
+            System.out.print("init");
+        }
     }
 }
