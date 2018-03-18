84bb01369820de966ead046d9e8729915d539069
Mahmoud Farghal <7odafarghal@gmail.com>
2018-03-18 22:08:52 +0200

    deleted keyboard

--- src/kika.java
+++
@@ -1,8 +0,0 @@
-public class kika{
-    public static void main(String[] args){
-        System.out.print("what is that keyboard");
-        if (keyboard.version = 1){
-            System.out.print("init");
-        }
-    }
-}
